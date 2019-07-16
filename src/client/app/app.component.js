515bc43a253c56f4dd0e15e6df34401e950a3948
Bari81 <alvaroeibeslopez@gmail.com>
2019-07-16 18:41:15 -0300

    limpiando codigo y css

--- src/client/app/app.component.js
+++ src/client/app/app.component.js
@@ -1,15 +1,4 @@
+import template from "./app.html";
 export const AppComponent = {
-  template: `
-    <header>            
-      <search-bar></search-bar>
-    </header>
-
-    <div class="content">   
-      <ui-view></ui-view>         
-    </div>
-
-    <footer>
-      Alvaro Eibes
-    </footer>
-  `
+  template
 };
